0d28d375c67f6d877959c902cdf83da4f8c36479
mikolyants1 <y.mikolyants@mail.ru>
2023-12-08 11:00:51 +0300

    d

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,8 @@ import express, { Request,Response,Express } from 'express'
 import cors from 'cors'
 import UserRouter from './routers/UserRouter.js'
 import DialogRouter from './routers/DialogRouter.js'
-import io from './socket.js';
+import { createServer } from 'http'
+import { Server }  from 'socket.io'
 export const Base:string = 'users.json';
 
 const PORT:string|number = process.env.PORT || 5000
@@ -17,13 +18,36 @@ app.use('/dialog',DialogRouter)
 
 app.use('/user',UserRouter)
 
-app.use((req:Request,res:Response):void=>{
-  res.redirect('/error')
-})
+const server = createServer(app);
 
-io.listen(5001);
+const io = new Server(server,{cors:{origin:"*"}});
 
-app.listen(PORT,():void=>{
+let users:number[] = [];
+let current:number = -1;
+
+function addUser(id:number):void{
+  users = Array.from(new Set([...users,id]));
+}
+
+function delUser(id:number):void{
+  users = users.filter((i:number)=>i!==id);
+}
+
+io.on("connection",(socket):void=>{
+  socket.on("join",(id:number):void=>{
+     current = id;
+     addUser(id);
+     socket.emit("online",users);
+   });
+
+  socket.on("disconnect",():void=>{
+     delUser(current);
+     socket.emit("online",users);
+   });
+
+ });
+server.listen(PORT,():void=>{
  console.log(`server works ,PORT ${PORT}`)
 })
+
 export default app
