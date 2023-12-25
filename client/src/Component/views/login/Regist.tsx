a9747c41715563ae863953ade457e3ba3c03a9b2
mikolyants1 <y.mikolyants@mail.ru>
2023-12-25 19:22:42 +0300

    g

--- client/src/Component/views/login/Regist.tsx
+++ client/src/Component/views/login/Regist.tsx
@@ -1,13 +1,10 @@
 import { useNavigate,NavigateFunction } from "react-router-dom"
 import { useState } from "react"
 import { EntryBlock,EntryTitle,EntryBut,LoginError } from '../../../style/style.js'
-import { bind, getCurrent, useAction, useAppSelector } from "../../../store/store.js"
 import { useAddUserMutation } from "../../../store/api/endpoints/UserEndpoints.js"
 import { data, stateUser } from "../../../types/type.js"
 import {useForm,FormProvider,SubmitHandler} from 'react-hook-form';
 import LoginCard from "../../ui/cards/logincards/LoginCard.js"
-import Loader from "../../ui/blocks/Loader.js"
-import Error from "../../ui/blocks/Error.js"
 import GetSuccess from "../../helpers/functions/GetSuccess.js"
 
 interface err{
@@ -20,15 +17,13 @@ interface Datas {
   isLoading:boolean
 }
 export default function Regist():JSX.Element{
-  const current:number = useAppSelector(getCurrent);
   const methods = useForm<stateUser>({
     defaultValues:{name:"",pass:""}
   });
   const {handleSubmit,reset} = methods;
   const navigate:NavigateFunction = useNavigate();
   const [error,setError] = useState<err>({show:false,mess:""});
-  const {setId,setPass}:bind = useAction();
-  const [addUser,{data,isError,isLoading}] = useAddUserMutation<Datas>();
+  const [addUser] = useAddUserMutation<Datas>();
 
   const check:SubmitHandler<stateUser>=async (date):Promise<void>=>{
     const {name,pass}:stateUser = date;
@@ -39,19 +34,14 @@ export default function Regist():JSX.Element{
       reset();
       return;
     }
-    if (!data) return;
-      setId(data.id);
-      setPass(pass);
       addUser(date);
-      navigate(`/page/main/${current}`);
+      navigate('/');
     } else {
       setError({show:true,mess:"no correct"});
       reset();
     };
   };
 
-  if (isLoading) return <Loader back="white" />;
-  if (isError) return <Error back="white" />
     return (
       <FormProvider {...methods}>
         <EntryBlock> 
--- server/src/routers/DialogRouter.ts
+++ server/src/routers/DialogRouter.ts
@@ -4,12 +4,13 @@ import addMess from '../controllers/dialog/addMess.js';
 import chanMess from '../controllers/dialog/chanMess.js';
 import delMess from '../controllers/dialog/delMess.js';
 import getMess from '../controllers/dialog/getMess.js';
+import Auth from '../middlewares/Auth.js';
 
 const router:Router = express.Router();
 
-router.post('/:id',validMess,check,addMess);
+router.post('/:id',validMess,check,Auth,addMess);
 
-router.put("/:id",validMess,check,chanMess);
+router.put("/:id",validMess,check,Auth,chanMess);
 
 router.delete("/:id",delMess);
 
